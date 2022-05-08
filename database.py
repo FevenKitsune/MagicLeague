2fb5f5d5d98e5197fa8350debfbf2835cb39bb7b
fevenkitsune <fevenkitsune@gmail.com>
2022-05-08 15:51:49 -0400

    implemented initial code for get and set

--- database.py
+++ database.py
@@ -18,9 +18,9 @@ def init() -> None:
     else:
         logger.warning("Configuration table was not found. Creating table...")
         init_cursor.execute("CREATE TABLE config {"
-                            "id     TEXT,"
-                            "name   TEXT,"
-                            "value  TEXT"
+                            "   id     TEXT,"
+                            "   name   TEXT,"
+                            "   value  TEXT"
                             "}")
         config_db.commit()
         logger.warning("Configuration table has been created.")
@@ -30,10 +30,23 @@ def init() -> None:
 
 
 def get_configuration(guild_id: str, parameter: str) -> str:
+    get_cursor = config_db.cursor()
+    fetched = get_cursor.execute(f"SELECT value "
+                                 f"FROM config "
+                                 f"WHERE id = {guild_id} AND name = {parameter} "
+                                 f"LIMIT 1")
+    get_cursor.close()
     return
 
 
-def set_configuration(guild_id: str, parameter: str, value: str) -> str:
+def set_configuration(guild_id: str, parameter: str, value: str) -> None:
+    set_cursor = config_db.cursor()
+    set_cursor.execute(f"DELETE FROM config "
+                       f"WHERE id = {guild_id} AND name = {parameter}")
+    set_cursor.execute(f"INSERT INTO config (id, name, value) "
+                       f"VALUES ({guild_id}, {parameter}, {value})")
+    config_db.commit()
+    set_cursor.close()
     return
 
 
